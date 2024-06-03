6fd1baff5a641991af47b0785306800aae2182eb
Dimas Saputra <dimas.maulanads@gmail.com>
2024-06-03 15:53:38 +0700

    fix unused import

--- src/grader.test.ts
+++ src/grader.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test } from 'bun:test';
+import { describe, expect, test } from 'bun:test';
 import { join } from 'node:path';
 import { main } from './grader';
 
